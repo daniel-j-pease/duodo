eef24b8a8f93892bb5f1b65acdcecc3031a7247a
Daniel Pease <daniel.j.pease@gmail.com>
2017-05-10 13:50:47 -0400

    first commit

---
+++ options.js
@@ -0,0 +1,71 @@
+$(document).ready(() => {
+  function setStorage() {
+    let target = Number($('#target').val());
+    let username = $('#username').val();
+    if (!Number.isInteger(target)) {
+      alert('gimme a number holmes');
+    } else {
+      chrome.storage.sync.set(
+        {
+          target: target,
+          username: username
+        },
+        () => {
+          $('#status').text(`target set to ${target}`);
+          setTimeout(() => {
+            $('#status').text('');
+          }, 750);
+        }
+      );
+    }
+  }
+
+  function restoreOptions() {
+    chrome.storage.sync.get(
+      {
+        target: target,
+        username: username
+      },
+      obj => {
+        typeof obj.username === 'string'
+          ? $('#username').val(`${obj.username}`)
+          : $('#username').val('');
+        typeof obj.target === 'number'
+          ? $('#target').val(`${obj.target}`)
+          : $('#target').val('');
+      }
+    );
+  }
+
+  function fetchData() {
+    let target = Number($('#target').val());
+    let username = $('#username').val();
+    return fetch(`http://www.duolingo.com/users/${username}`)
+      .then(r => r.json())
+      .then(data => {
+        console.log('fetching:', data);
+        // most recent session is the last thing in the array.
+        // each 'improvement' is broken down into its own item with its own timestamp
+      });
+  }
+
+  $('#save').click(() => {
+    setStorage();
+    fetchData();
+  });
+
+  $('#check').click(() => {
+    chrome.storage.sync.get(obj => {
+      console.log('checking storage:', obj);
+      restoreOptions();
+    });
+  });
+
+  $('#clear').click(() => {
+    chrome.storage.sync.remove(['username', 'target'], obj => {
+      console.log('storage cleared:', obj);
+    });
+  });
+
+  restoreOptions();
+});
