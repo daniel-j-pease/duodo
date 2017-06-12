261359bbd1c4a23e0c0dce9a3d24b6ba34367400
Daniel Pease <daniel.j.pease@gmail.com>
2017-06-12 11:36:23 +0100

    webRequest attempt two # <- Subject message. Maximum 50 characters.   ->| # # <- After two line breaks come the rest of the lines, or the body.  ->| # <- The maximum line length for the body is 72 characters.          ->| # # For more info on how to write a good commit message, see: # http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html

--- background/background.js
+++ background/background.js
@@ -1,86 +1,35 @@
 ('use strict');
-window.onload = function() {
+window.onload = () => {
   console.log('background js running');
-  (() => {
+
+  function getUrls() {
     chrome.storage.sync.get(obj => {
-      typeof obj.duoDo_sites === 'undefined'
-        ? (() => {
-            console.log('no sites');
-            updateFilters(void 0, 'abc');
-            return;
-          })()
-        : (() => {
-            console.log('yes sites');
-            updateFilters(obj.duoDo_sites);
-            return;
-          })();
+      console.log('bg onload storage', obj);
     });
-  })();
-
-  (() => {
-    chrome.storage.onChanged.addListener((changes, namespace) => {
-      console.log('changes.duoDo_sites.newValue', changes.duoDo_sites.newValue);
-      typeof changes.duoDo_sites.newValue === 'undefined'
-        ? (() => {
-            console.log('newVal undef');
-            updateFilters([]);
-            return;
-          })()
-        : (() => {
-            console.log('newVal def');
-            updateFilters(changes.duoDo_sites.newValue);
-          })();
-    });
-  })();
-
-  function updateFilters(urls) {
-    chrome.storage.sync.get(obj => {
-      console.log('obj', obj);
-    });
-    console.log('arg', urls);
-    if (typeof urls === 'undefined') {
-      console.log('args undef');
-      return;
-    }
-    let urlObj = {
-      urls: [],
-      types: [
-        'main_frame',
-        'sub_frame',
-        'stylesheet',
-        'script',
-        'image',
-        'object',
-        'xmlhttprequest',
-        'other'
-      ]
-    };
-
-    for (let i = 0; i < urls.length; i++) {
-      urlObj.urls.push(urls[i]);
-    }
-
-    chrome.webRequest.onBeforeRequest.addListener(
-      details => {
-        for (let i = 0; i < urlObj.urls.length; i++) {
-          if (details.url.includes(urlObj.urls[i].slice(6, -6))) {
-            console.log('includes');
-            if (details.type === 'main_frame') {
-              chrome.tabs.create({
-                url: 'public/blocked/index.html',
-                active: true
-              });
-              chrome.tabs.remove(details.tabId);
-            }
-            return { cancel: true };
-          } else {
-            console.log('doesnt include');
-            return;
-          }
-        }
-      },
-      urlObj,
-      ['blocking']
-    );
   }
+
+  (function listenForChanges() {
+    console.log('listening');
+    chrome.storage.onChanged.addListener((changes, namespace) => {
+      console.log('cn', changes, namespace);
+      applyBlock(changes.duoDo_sites.newValue);
+    });
+  })();
+
+  function block(object details) {
+    console.log('db ca', changes, area);
+  }
+
+  function applyBlock() {
+    console.log('block applied');
+    chrome.webRequest.onBeforeRequest.addListener(details => {
+      block(changes, area);
+    });
+  }
+
+  function removeBlock() {
+    console.log('block removed');
+    chrome.webRequest.onBeforeRequest.removeListener(block);
+  }
+  getUrls();
 };
---
+++ background_backup.js
@@ -0,0 +1,87 @@
+('use strict');
+window.onload = function() {
+  console.log('background js running');
+  (() => {
+    chrome.storage.sync.get(obj => {
+      typeof obj.duoDo_sites === 'undefined'
+        ? (() => {
+            console.log('no sites');
+            updateFilters(void 0, 'abc');
+            return;
+          })()
+        : (() => {
+            console.log('yes sites');
+            updateFilters(obj.duoDo_sites);
+            return;
+          })();
+    });
+  })();
+
+  (() => {
+    chrome.storage.onChanged.addListener((changes, namespace) => {
+      console.log('changes.duoDo_sites.newValue', changes.duoDo_sites.newValue);
+      typeof changes.duoDo_sites.newValue === 'undefined'
+        ? (() => {
+            console.log('newVal undef');
+            updateFilters([]);
+            return;
+          })()
+        : (() => {
+            console.log('newVal def');
+            updateFilters(changes.duoDo_sites.newValue);
+          })();
+    });
+  })();
+
+  function updateFilters(urls) {
+    chrome.storage.sync.get(obj => {
+      console.log('obj', obj);
+    });
+    console.log('arg', urls);
+    if (typeof urls === 'undefined') {
+      console.log('args undef');
+      return;
+    }
+    let urlObj = {
+      urls: [],
+      types: [
+        'main_frame',
+        'sub_frame',
+        'stylesheet',
+        'script',
+        'image',
+        'object',
+        'xmlhttprequest',
+        'other'
+      ]
+    };
+
+    for (let i = 0; i < urls.length; i++) {
+      urlObj.urls.push(urls[i]);
+    }
+
+    chrome.webRequest.onBeforeRequest.addListener(
+      details => {
+        console.log('details', details);
+        for (let i = 0; i < urlObj.urls.length; i++) {
+          if (details.url.includes(urlObj.urls[i].slice(6, -6))) {
+            console.log('includes', urlObj.urls[i]);
+            if (details.type === 'main_frame') {
+              chrome.tabs.create({
+                url: 'public/blocked/index.html',
+                active: true
+              });
+              chrome.tabs.remove(details.tabId);
+            }
+            return { cancel: true };
+          } else {
+            console.log('doesnt include');
+            return;
+          }
+        }
+      },
+      urlObj,
+      ['blocking']
+    );
+  }
+};
