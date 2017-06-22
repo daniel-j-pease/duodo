c2d8875ef8705b7603c357c538da248075103d8b
Daniel Pease <daniel.j.pease@gmail.com>
2017-06-22 15:58:49 -0400

    finalize fix for add remove block listener # <- Subject message. Maximum 50 characters.   ->| # # <- After two line breaks come the rest of the lines, or the body.  ->| # <- The maximum line length for the body is 72 characters.          ->| # # For more info on how to write a good commit message, see: # http://tbaggery.com/2008/04/19/a-note-about-git-commit-messages.html

--- background/background.js
+++ background/background.js
@@ -1,29 +1,9 @@
 ('use strict');
 window.onload = () => {
-  console.log('background js running');
-
-  (function getUrls() {
-    console.log('getting');
-    chrome.storage.sync.get(obj => {
-      console.log('bg onload storage', obj);
-    });
-  })();
+  console.log('************************ background js running');
 
   (function listenForChanges() {
-    chrome.storage.onChanged.addListener((changes, namespace) => {
-      console.log('cn', changes, namespace);
-      if (changes.duoDo_sites.newValue) {
-        console.log('new value!');
-        applyBlock(changes.duoDo_sites.newValue);
-      } else {
-        console.log('no new value!');
-        applyBlock([]);
-      }
-    });
-  })();
-
-  function applyBlock(urlArr) {
-    let urlObj = {
+    urlObj = {
       urls: [],
       types: [
         'main_frame',
@@ -36,33 +16,33 @@ window.onload = () => {
         'other'
       ]
     };
+    chrome.storage.onChanged.addListener((changes, namespace) => {
+      if (changes.duoDo_sites.newValue) {
+        urlObj.urls = changes.duoDo_sites.newValue;
+        chrome.webRequest.onBeforeRequest.removeListener(blockLogic);
+        chrome.webRequest.onBeforeRequest.addListener(blockLogic, urlObj, [
+          'blocking'
+        ]);
+      } else {
+        urlObj.urls = [];
+        chrome.webRequest.onBeforeRequest.removeListener(blockLogic);
+      }
+    });
+  })();
 
-    for (let i = 0; i < urlArr.length; i++) {
-      urlObj.urls.push(urlArr[i]);
+  const blockLogic = details => {
+    for (let i = 0; i < urlObj.urls.length; i++) {
+      console.log('urlobj.urls', urlObj.urls);
+      if (details.url.includes(urlObj.urls[i].slice(6, -6))) {
+        chrome.tabs.query({ active: true }, tabs => {
+          chrome.tabs.remove(tabs[0].id);
+          chrome.tabs.create({
+            url: 'public/blocked/index.html',
+            active: true
+          });
+        });
+        return { cancel: true };
+      }
     }
-
-    chrome.webRequest.onBeforeRequest.addListener(
-      details => {
-        console.log('details', details);
-        for (let i = 0; i < urlObj.urls.length; i++) {
-          if (details.url.includes(urlObj.urls[i].slice(6, -6))) {
-            console.log('includes', urlObj.urls[i]);
-            if (details.type === 'main_frame') {
-              chrome.tabs.create({
-                url: 'public/blocked/index.html',
-                active: true
-              });
-              chrome.tabs.remove(details.tabId);
-            }
-            return { cancel: true };
-          } else {
-            console.log('doesnt include');
-            return;
-          }
-        }
-      },
-      urlObj,
-      ['blocking']
-    );
-  }
+  };
 };
